7a1f556c023c75bd87a76ecfe4b98320487d6ba1
umugabo <mugaboyves1@gmail.com>
2022-04-15 13:55:07 +0200

    made changes

--- student/filters.py
+++ student/filters.py
@@ -5,13 +5,13 @@ from .models import *
 
 class StudentFilter(django_filters.FilterSet):
     f_name = CharFilter(field_name="f_name", lookup_expr='icontains')
-    year_reg = CharFilter(field_name="year_reg", lookup_expr='icontains')
+    # year_reg = CharFilter(field_name="year_reg", lookup_expr='icontains')
     # start_date = DateFilter(field_name="dob", lookup_expr='gte')
     # end_date = DateFilter(field_name="dob", lookup_expr='lte')
     class Meta:
          model = Student
          fields = '__all__'
-         exclude = ['year_reg','l_name','dob', 'service_category', 'st_image']
+         exclude = ['year_reg','l_name','dob','province','district', 'service_category','father_name','mother_name','sectors','cell','village','note','year_reg', 'st_image']
 
 class TeacherFilter(django_filters.FilterSet):
     f_name = CharFilter(field_name="f_name", lookup_expr='icontains')
